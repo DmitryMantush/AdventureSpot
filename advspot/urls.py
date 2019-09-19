62b412931c6e925c6dcfb4dc7bd1d92ab0e56d2b
DmitryMantush <brownmittens@gmail.com>
2019-09-19 14:19:14 +0300

    Users can create, update posts now. Login check at the post creation.

--- advspot/urls.py
+++ advspot/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 
 urlpatterns = [
-    path('', views.home, name='spot-home'),
+    path('', PostListView.as_view(), name='spot-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('about/', views.about, name='spot-about'),
 ]
--- advspot/views.py
+++ advspot/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
@@ -7,6 +9,35 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'advspot/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'advspot/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
